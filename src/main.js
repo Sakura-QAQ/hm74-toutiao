a5940ddb2f00fecac64b56fe930361d8414f55b9
cxk <1005833@qq.com>
2019-07-11 21:46:33 +0800

    使用element-ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import EleMent from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(EleMent)
+
 Vue.config.productionTip = false
 
 new Vue({
